405340da431044001085c78fa1464b66873e81fc
Your Name <tranhoangloc502@gmail.com>
2023-10-14 20:32:59 +0700

    done UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./component/navbar/Navbar";
+import Register from "./page/register/Register";
+import Settings from "./page/settings/Settings";
+import Login from "./page/login/Login";
+import Home from "./page/home/Home";
+import Single from "./page/single/Single";
+import Write from "./page/write/Write";
+import { BrowserRouter as Router, Routes ,Route } from "react-router-dom";
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const currentUser = false;
+    return (
+        <>
+            <Router>
+                <Navbar />
+                <Routes>
+
+                    <Route exact path="/" element={<Home />}/>
+
+
+
+
+                    <Route path="/posts"  element={<Home />}/>
+
+
+
+                    <Route path="/register" element={currentUser ? <Home /> : <Register />}/>
+
+
+
+
+                    <Route path="/login" element={currentUser ? <Home /> : <Login />}/>
+
+
+                    <Route path="/post/:id" element={<Single />}/>
+
+
+
+
+                    <Route path="/write" element={currentUser ? <Write /> : <Login />}/>
+
+
+                    <Route path="/settings" element={currentUser ? <Settings /> : <Login />} />
+
+
+
+                </Routes>
+            </Router>
+        </>
+    );
 }
 
 export default App;
